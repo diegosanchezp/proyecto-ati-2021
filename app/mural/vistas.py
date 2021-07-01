62426e4335b8a01640f2b2c9861fa021af78faa4
Diego Sánchez <diegosandmg@gmail.com>
2021-06-30 21:08:49 -0400

    rename module views file

--- app/mural/vistas.py
+++ app/mural/vistas.py
@@ -2,12 +2,13 @@ from flask import Blueprint, render_template
 mural_blueprint = Blueprint('mural_blueprint', __name__, template_folder='templates')
 
 
+from app import db
+
 @mural_blueprint.route("/")
 def index():
     """
     Vista principal del mural
     """
-    from app import db
     return render_template("mural/mural.html")
 
 @mural_blueprint.route("/publicaciones")
