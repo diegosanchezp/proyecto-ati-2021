cb37a9a83085a5a31ad8b34e0f521e0104fa2f79
Cesar Salazar <csalazar@shokworks.io>
2021-07-09 21:04:42 -0430

    Feature: Mural y Detalle Publicacion

---
+++ src/app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+import flask
+from pymongo import MongoClient
+
+app = flask.Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    client = MongoClient('mongodb',27017)
+    try:
+        client.admin.command('ismaster')
+        return 'Database working\n'
+    except Exception:
+        return 'Database is not working\n'
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000, debug=True)     # open for everyone
