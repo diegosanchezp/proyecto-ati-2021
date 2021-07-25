90c56f8e58b9100ba900b30a1b41605f07034287
Diego Sánchez <diegosandmg@gmail.com>
2021-07-25 09:05:53 -0400

    feat: User Config models

--- app/models/user.py
+++ app/models/user.py
@@ -1,10 +1,45 @@
+"""
+Modelos relacionados con usuarios
+"""
+
 from flask_user import UserMixin
 from app import db
 
+
+
+class UserNotificationsConfig(db.EmbeddedDocument):
+    """
+    Configuracion personal de las notificaciones
+
+    Todas estas notificaciones le llegaran al usuario y las podrá
+    ver en la vista de notificaciones
+    """
+    comentarios = db.BooleanField(default=True)
+    mensajes_chat = db.BooleanField(default=True)
+    amigos_conectados = db.BooleanField(default=True)
+
+    def __str__(self):
+        return f"comentarios={self.comentarios} mensajes_chat={self.mensajes_chat}"
+
+class UserConfig(db.Document):
+    """
+    Configuracion de un usuario
+    """
+    color_perfil = db.StringField()
+    color_muro = db.StringField()
+    publicaciones_privadas = db.BooleanField(default=False)
+    notificaciones = db.EmbeddedDocumentField("UserNotificationsConfig")
+
 class User(db.Document, UserMixin):
+    """
+    Modelo usuario
+    """
+
     # User authentication information
     username = db.StringField(unique=True)
     password = db.StringField()
+
+    # Fields pedido en los requerimientos
     nombre = db.StringField()
     foto = db.ImageField(size=(1024, 768, True))
     email = db.EmailField()
@@ -17,12 +52,22 @@ class User(db.Document, UserMixin):
     musica = db.StringField()
     video_juegos = db.ListField()
     lenguajes_programacion = db.ListField()
+    config = db.ReferenceField("UserConfig")
 
     def __init__(self, *args, **kwargs):
+        """
+        Extending this method to put some default field values
+        """
+
         super().__init__(*args, **kwargs)
+
         # Create username from nombre
         if self.username is None and self.nombre is not None:
             self.username = self.nombre.lower().replace(" ", "_")
+
+        # Create default configuration
+        if self.config is None:
+            self.config = UserConfig(notificaciones=UserNotificationsConfig())
 
     def __str__(self):
         return f"username={self.username} nombre={self.nombre} ci={self.ci}"
