cc3308775e736b25c915355b48322758453c2f63
Diego Sánchez <diegosandmg@gmail.com>
2021-07-26 22:32:36 -0400

    feat: modelo peticiones, notificaciones
    feat: modulo para constantes

---
+++ app/models/peticion.py
@@ -0,0 +1,105 @@
+from app import db
+from enum import Enum
+from abc import ABC
+
+class AbstractModel(db.Document):
+    """
+    Clase abstracta para modelos que utilizan maquinas de
+    estado finito, requiere de las propiedades self.estado
+    y self.maquina_estado
+    """
+
+    meta = {
+        'abstract': True,
+    }
+
+    descripcion = db.StringField()
+    emisor = db.ReferenceField("User")
+    receptor = db.ReferenceField("User")
+    def transition(self, event) -> None:
+        """
+        Set the next state, given an event
+        """
+        try:
+            next_state = self.maquina_estado[self.estado][event]
+        except KeyError:
+            raise Exception(
+                f"Error: transicion no definida para estado: {self.estado} con event: {event}"
+            ) from KeyError
+
+        self.estado = next_state
+
+class PeticionEstado(Enum):
+    """
+    Posibles estados de una petición
+    """
+    ACEPTADA = "aceptada"
+    RECHAZADA = "rechazada"
+    ESPERA = "en espera"
+
+class TipoPeticiones(Enum):
+    CHAT = "peticion para chatear"
+    AMISTAD = "solicitud de amistad"
+
+class PeticionEnvento(Enum):
+    ACEPTAR = "aceptar"
+    RECHAZAR = "rechazar"
+
+class Peticion(AbstractModel):
+    """
+    Modelo de peticion
+    """
+    estado = db.EnumField(
+        PeticionEstado,
+        default=PeticionEstado.ESPERA
+    )
+
+    maquina_estado = {
+        PeticionEstado.ESPERA: {
+            PeticionEnvento.ACEPTAR: PeticionEstado.ACEPTADA,
+            PeticionEnvento.RECHAZAR: PeticionEstado.RECHAZADA,
+        }
+    }
+
+    tipo = db.EnumField(TipoPeticiones, required=True)
+
+#TIPO_NOTIFICACIONES = (
+#    ("MENSAJE_CHAT", "mensaje chat"),
+#    ("COMENTARIO", "comentario"),
+#    ("AMIGO_CONECTADO", "amigo conectado"),
+#)
+
+class NotificacionEstado(Enum):
+    """
+    Estados de notificaciones
+    """
+    LEIDA = "leida"
+    NO_LEIDA = "no leida"
+
+class TipoNotificaciones(Enum):
+    """
+    Posibles tipo de notificaciones
+    """
+    MENSAJE_CHAT="mensaje chat"
+    COMENTARIO = "comentario"
+    AMIGO_CONECTADO = "amigo conectado"
+
+class NotiEvento(Enum):
+    LEER = "leer"
+
+class Notificacion(AbstractModel):
+    """
+    Modelo de notificaciones
+    """
+    estado = db.EnumField(
+        NotificacionEstado,
+        default=NotificacionEstado.NO_LEIDA
+    )
+
+    maquina_estado = {
+        NotificacionEstado.NO_LEIDA: {
+            NotiEvento.LEER: NotificacionEstado.LEIDA
+        }
+    }
+
+    tipo = db.EnumField(TipoNotificaciones, required=True)
