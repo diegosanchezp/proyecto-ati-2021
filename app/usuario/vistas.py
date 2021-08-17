ef75ab058cc14823b4541e6240d4b587a6275a3d
Diego Sánchez <diegosandmg@gmail.com>
2021-08-16 23:13:33 -0400

    feat: delete friend
    feat: get number of friends
    refactor: code from last commits

--- app/usuario/vistas.py
+++ app/usuario/vistas.py
@@ -1,6 +1,11 @@
-from flask import Blueprint, render_template
+from flask import (
+    Blueprint, render_template,
+    request, flash,
+)
 from app.models.user import USUARIO_GENEROS
 from flask_login import current_user
+from flask_babel import _
+
 usuario_blueprint = Blueprint('usuario_blueprint', __name__, template_folder='templates')
 from app.models.user import User
 
@@ -20,31 +25,27 @@ def recuperar_token():
 
     return render_template("usuario/recuperar_pass_token_enviado.html")
 
-@usuario_blueprint.route("/ver-perfil/<username>")
+@usuario_blueprint.route("/ver-perfil/<username>", methods=["POST", "GET"])
 def ver_perfil(username):
     """
     Vista de ver perfil
     """
-    target_user = User.objects(username=username).first()
+    target_user = User.objects.get_or_404(username=username)
 
-    target_user_format = {
-        'nombre' : target_user.nombre,
-        'email' : target_user.email,
-        'foto' : target_user.get_foto_url(),
-        'ci' :  target_user.ci,
-        'fecha_nacimiento' : target_user.fecha_nacimiento,
-        'genero' : USUARIO_GENEROS[1][1] if target_user.genero=='F' else USUARIO_GENEROS[0][1],
-        'descripcion' : target_user.descripcion,
-        'color' : target_user.color,
-        'video_juegos' : ', '.join(target_user.video_juegos),
-        'lenguajes_programacion' : ', '.join(target_user.lenguajes_programacion)
-        } 
+    it_is_the_current_user = target_user == current_user
 
-    it_is_the_current_user = False
-    if target_user.username == current_user.username:
-        it_is_the_current_user = True
-    
-    return render_template("usuario/ver_perfil.html", target_user = target_user_format, it_is_the_current_user = it_is_the_current_user)
+    if request.method == "POST" and request.form["action"] == "BORRAR AMIGO":
+        # Delete a friend
+        if not it_is_the_current_user:
+            flash(_("Amistad borrada"), 'success')
+            current_user.amigos.remove(target_user)
+            current_user.save()
+
+    return render_template("usuario/ver_perfil.html",
+        target_user = target_user,
+        it_is_the_current_user = it_is_the_current_user,
+        is_friend=target_user in current_user.amigos
+    )
 
 @usuario_blueprint.route("/editar-privacidad")
 def editar_privacidad():
