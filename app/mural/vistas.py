6b9396a620619924f46d0b7ec4ea5ad9d40a34a6
Diego Sánchez <diegosandmg@gmail.com>
2021-09-27 22:35:36 -0400

    feat: better notification messages

--- app/mural/vistas.py
+++ app/mural/vistas.py
@@ -79,7 +79,7 @@ def detalle_publicacion(publicacionID: str):
         n = Notificacion(
             emisor=current_user,
             receptor=publicacion.autor,
-            descripcion=f"{current_user.nombre} ha comentado tu publicación",
+            descripcion=f"ha comentado tu publicación",
             tipo=TipoNotificaciones.COMENTARIO,
             recurso=publicacion,
         )
@@ -115,7 +115,7 @@ def comentar_comentario(comentarioID: str):
         n = Notificacion(
             emisor=current_user,
             receptor=comentario.usuario,
-            descripcion=f"{current_user.nombre} ha respondido tu comentario de la publicacion",
+            descripcion=f"ha respondido tu comentario de la publicacion",
             tipo=TipoNotificaciones.COMENTARIO,
             recurso=comentario.publicacion,
         )
